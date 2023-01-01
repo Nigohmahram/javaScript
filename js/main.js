534256d5a19f62ea5a460f6d8b490145bc228471
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-01-02 03:50:30 +0900

    js plain

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// "use strict"; Qat'iy rejimda ishlash
+
+// let qiymati o'zgarucchan
+// let number = 10;
+// const qiymati qisman  o'zgarmaydi
+// const borderColor = "red";
+
+// number = 20;
+// console.log(number);
+//Number
+// let number = 5.6; //Data type = number
+// console.log(8 / 0); //infinity
+// console.log("Khojiakbar" * 8); //NaN =Not a number
+
+
+//String
+// const clientName = `Khojiakbar`;
+
+//Boolean
+// const isMarrid = false; //Ha |to'g'ri
+// const isCricleEart = true; //yo'q |noto'g'ri
+
+//Null |difined hech qanday o'zgaruvchi yo'q bo'lganda chiqadi | yo'q bo'lgan o'zgaruvchini ishlatsak Null chiqadi
+// console.log(clienrAge);
+
+//Undifined | O'zgaruvchida hech qanday qiymat bo'lmaganda chiqaradi | O'zgaruvchi bor lekin hech qanday qiymat bolmaganda Undifined chiqaradi
+// let und;
+// console.log(und);
+
+//Object |Ma'lumotlarni bir joyda saqlash, bir qutida ko'p ma'lumotlarni joylash
+// const thief = {
+//      //key:  value
+//      age: 30, //number
+//      jacket: "black", //string
+//      isLong: false,//boolean
+// };
+// console.log(thief.age);
+
+
+//array
+// const colors = ["red", "black", "blue", 10, [], {}];
+// console.log(colors[1]);
+
+// alert("Hello JavaScript");
+// const prace = confirm("Are you here?");
+// console.log(prace);
+
+// const currentPlace = prompt("Siz shu yerdamisiz?", "ha shu yerdaman");
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you?", "18");
+// console.log( age + 10);
+
+// const favoriteColor = [];
+// favoriteColor[0] = prompt("Sizning eng yaxshi ko'rgan kishilaringiz #1", "");
+// favoriteColor[1] = prompt("Sizning eng yaxshi ko'rgan kishilaringiz #2", "");
+// favoriteColor[2] = prompt("Sizning eng yaxshi ko'rgan kishilaringiz #3", "");
+// console.log(favoriteColor);
+
+// const channel = prompt("Sizning yoqtirgan YouTube kanalingiz?");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Khojiakbar";
+// const work = "developer";
+// console.log(`User name is ${user}, he is ${work}`);
+
+//incr/decr
+// let incr = 10;
+// let decr = 10;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 3);// == bo'lsa qiymatni solishtiradi | === ta bo'sa qiymat va malumot turini solishtiradi
+// console.log(3 * 5 == 25);
+// console.log(5 * 5 === "25");
+
+// && Va || Yoki
+// && VA ichida birdona false bo'lsaham false ko'rsatadi!
+// const isAge = true;
+// const isClose = true;
+// const isCheld = false;
+// console.log(isAge && isClose && isCheld);
+// // || YOKI ichida birdona true bo'lsaham true ko'rsatadi!
+// const isBge = true;
+// const isVlose = true;
+// const isSheld = false;
+// console.log(isBge || isVlose || isSheld);
+
+// console.log(2 + 2 * 2 !== 6);
+// console.log(2 + 2 * 2 !== 8);
+// console.log(4 * 3 *  3 != 108);
